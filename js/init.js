f5a622225f39454d4a6e111b7a3eddaf5766e493
Micah Werbitt <micah@werbitt.net>
2015-05-30 14:30:01 -0400

    Start adding the map

--- js/init.js
+++ js/init.js
@@ -1,8 +1,19 @@
+function initMap() {
+    var mapCanvas = document.getElementById('map-canvas');
+    var mapOptions = {
+      center: new google.maps.LatLng(41.104051,-73.82866),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+  }
+
 (function($){
   $(function(){
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
 
+    initMap();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
