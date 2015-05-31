365d3dd4437bb2bee86a48947fa42444cc516804
Micah Werbitt <micah@werbitt.net>
2015-05-30 20:48:39 -0400

    Remove maps code from init.js

--- js/init.js
+++ js/init.js
@@ -1,19 +1,7 @@
-function initMap() {
-    var mapCanvas = document.getElementById('map-canvas');
-    var mapOptions = {
-      center: new google.maps.LatLng(41.104051,-73.82866),
-      zoom: 15,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-    var map = new google.maps.Map(mapCanvas, mapOptions);
-  }
-
 (function($){
   $(function(){
-
     $('.button-collapse').sideNav({closeOnClick: true});
     $('.parallax').parallax();
     $('.scrollspy').scrollSpy();
-    //initMap();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
