f67b4003d8828350857c3d45faab5ebb38189228
Micah Werbitt <micah@werbitt.net>
2015-05-30 21:01:59 -0400

    Add scroll blocking to the map

--- js/init.js
+++ js/init.js
@@ -1,7 +1,13 @@
 (function($){
-  $(function(){
-    $('.button-collapse').sideNav({closeOnClick: true});
-    $('.parallax').parallax();
-    $('.scrollspy').scrollSpy();
-  }); // end of document ready
+    $(function(){
+        $('.button-collapse').sideNav({closeOnClick: true});
+        $('.parallax').parallax();
+        $('.scrollspy').scrollSpy();
+        $('#map-canvas').click(function () {
+            $('#map-canvas iframe').css("pointer-events", "auto");
+        });
+        $('#map-canvas').mouseleave(function () {
+            $('#map-canvas iframe').css("pointer-events", "none");
+        });
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
